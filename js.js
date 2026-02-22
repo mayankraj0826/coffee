3aa1293ce92935a5825876b0fb2c6a766b145156
mayankraj0826 <mayankraj082605@gmail.com>
2026-02-22 09:08:12 +0530

    Update and rename js.js to script.js

--- js.js
+++
@@ -1,1 +0,0 @@
-console.log("Brew Haven website loaded");
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Brew Haven website loaded");
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  toggleBtn.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
